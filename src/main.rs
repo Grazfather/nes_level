2b4f62c6f75c7ff2c43ae569a90b02a21ece4c02
Grazfather <grazfather@gmail.com>
2016-09-12 22:04:33 -0700

    Make filename setting more idiomatic

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,13 @@ use std::env;
 
 
 fn main() {
-    let mut filename: String;
-    if std::env::args().len() > 1 {
-        filename = std::env::args().nth(1).unwrap();
-    } else {
-        filename = "test.nes".to_string();
+    let filename = if env::args().len() > 1 {
+        env::args().nth(1).unwrap()
     }
+    else {
+        "test.nes".to_string()
+    };
+
     let mut cpu: cpu::CPU = cpu::CPU::new(&filename);
 
     println!("Initializing CPU with state:");
