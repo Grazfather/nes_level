24b99c73e801f2243c56cf1d86bf23db6a328497
Grazfather <grazfather@gmail.com>
2017-04-30 14:15:28 -0700

    Add argument parser (that only takes one arg type)

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,30 @@ mod rom;
 
 use std::env;
 
+#[derive(Debug)]
+pub struct Args {
+    filename: String,
+}
+
+impl Args {
+    fn parse_args() -> Result<Args, &'static str> {
+        let mut args = Args{ filename: "test.nes".to_string() };
+
+        for arg in env::args() {
+            match arg {
+                _ => {
+                    args.filename = arg;
+                }
+            }
+        }
+        return Ok(args)
+    }
+}
 
 fn main() {
-    let filename = if env::args().len() > 1 {
-        env::args().nth(1).unwrap()
-    }
-    else {
-        "test.nes".to_string()
-    };
+    let args = Args::parse_args().unwrap();
 
-    let mut cpu: cpu::CPU = cpu::CPU::new(&filename);
+    let mut cpu: cpu::CPU = cpu::CPU::new(&args.filename);
     cpu.reset();
 
     println!("Initializing CPU with state:");
